8d365b9d7ec883616a935b06b7a5ee7ff793f574
Anett Balla <anett0501@gmail.com>
2026-09-25 19:28:06 +0200

    Task 10 - calculate movingAverage

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -4,6 +4,7 @@ package com.aldisued.iot.monitoring.service;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -14,6 +15,7 @@ public class MeasurementCalculatorService {
     if (deviation == null || deviation < 0.0 || deviation > 1.0) {
       throw new IllegalArgumentException("deviation must be in range [0.0, 0.1]");
     }
+
     if (CollectionUtils.isEmpty(values)) {
       return List.of();
     }
@@ -28,8 +30,34 @@ public class MeasurementCalculatorService {
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
-    // TODO: Task 10
-    return List.of();
+
+    validateMovingAverageInputs(data, windowSize);
+
+    double sum = data.stream().limit(windowSize).mapToDouble(Double::doubleValue).sum();
+
+    List<Double> movingAverages = new ArrayList<>();
+    movingAverages.add(sum / windowSize);
+
+    for (int i = windowSize; i < data.size(); i++) {
+      sum += data.get(i) - data.get(i - windowSize);
+      movingAverages.add(sum / windowSize);
+    }
+
+    return movingAverages;
   }
 
+  private void validateMovingAverageInputs(List<Double> data, int windowSize) {
+    if (windowSize <= 0 ) {
+      throw new IllegalArgumentException("windowSize must be positive");
+    }
+    if (CollectionUtils.isEmpty(data)) {
+      throw new IllegalArgumentException("data cannot be empty");
+    }
+    if (data.size() < windowSize) {
+      throw new IllegalArgumentException("windowSize is greater than the number of values");
+    }
+    if (data.stream().anyMatch(Objects::isNull))  {
+      throw new IllegalArgumentException("none of the values can be null");
+    }
+  }
 }
