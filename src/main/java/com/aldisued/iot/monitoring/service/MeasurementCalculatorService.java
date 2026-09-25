6014b12d64bd5f90b8898499006108bda4312a23
Anett Balla <anett0501@gmail.com>
2026-09-25 19:09:36 +0200

    Task 9 - filter measurements by average deviation

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -1,15 +1,30 @@
 package com.aldisued.iot.monitoring.service;
 
 
-import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MeasurementCalculatorService {
 
   public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
+    if (deviation == null || deviation < 0.0 || deviation > 1.0) {
+      throw new IllegalArgumentException("deviation must be in range [0.0, 0.1]");
+    }
+    if (CollectionUtils.isEmpty(values)) {
+      return List.of();
+    }
+
+    double average = values.stream().filter(Objects::nonNull)
+            .mapToDouble(Double::doubleValue).average()
+            .orElseThrow(() -> new IllegalArgumentException("Average cannot be calculated"));
+    double lowerBound = average - average * deviation;
+    double upperBound = average + average * deviation;
+
+    return values.stream().filter(v -> v >= lowerBound && v <= upperBound).toList();
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
