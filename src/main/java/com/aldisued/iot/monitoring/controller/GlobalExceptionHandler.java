e0eb233bb831af0e7f1155aa1eb7fb963e3aeb61
Anett Balla <anett0501@gmail.com>
2026-09-25 18:23:55 +0200

    Task 4 - sensor creation error handling

---
+++ src/main/java/com/aldisued/iot/monitoring/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.aldisued.iot.monitoring.controller;
+
+import com.aldisued.iot.monitoring.exception.SensorAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SensorAlreadyExistsException.class)
+    public ResponseEntity<?> handleSensorAlreadyExistsException(SensorAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+}
